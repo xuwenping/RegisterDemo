13761ae383a2636677fa61f394b3a26e54832707
xuwenping <xuwenping@rongyi.com>
2017-03-07 18:29:15 +0800

    实现了一个注册界面

---
+++ app/src/main/java/com/example/administrator/resgistdemo/RegisterSuccessActivity.java
@@ -0,0 +1,13 @@
+package com.example.administrator.resgistdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RegisterSuccessActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_success);
+    }
+}
