bb494bd7ddc256e266bef8d3a683335dc514f748
devinxu <xuwenping3211@gmail.com>
2017-03-09 23:06:03 +0800

    完成了登陆页面的button处理

--- app/src/main/java/com/example/administrator/resgistdemo/TitleLayout.java
+++ app/src/main/java/com/example/administrator/resgistdemo/TitleLayout.java
@@ -19,15 +19,44 @@ public class TitleLayout extends LinearLayout implements View.OnClickListener{
 
     private TextView titleContent;
 
+    private Button loginBtn;
+
     public TitleLayout(Context context, AttributeSet attribute) {
         super(context, attribute);
         LayoutInflater.from(context).inflate(R.layout.title, this);
 
+        closeBtn = (Button) findViewById(R.id.close_btn);
+        closeBtn.setOnClickListener(this);
 
+        actionBtn = (Button) findViewById(R.id.action_btn);
+        actionBtn.setOnClickListener(this);
+
+
+
+        titleContent = (TextView) findViewById(R.id.title_content);
     }
 
     @Override
     public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.close_btn:
+                ((LoginActivity) getContext()).finish();
+                break;
 
+            case R.id.action_btn:
+
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    public void setActionBtnText(String text) {
+        actionBtn.setText(text);
+    }
+
+    public void setTitleContent(String text) {
+        titleContent.setText(text);
     }
 }
